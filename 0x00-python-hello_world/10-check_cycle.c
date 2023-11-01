61717e26b287fd9a079fbfe9ece710eb7afc7242
nanakwasi-0 <nankwasi999@gmail.com>
2023-11-01 15:36:24 -0700

    Interview prep

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,10 +8,10 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *fast = list;
-	
+
 	if (!list)
 		return (0);
-	
+
 	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
@@ -19,6 +19,6 @@ int check_cycle(listint_t *list)
 		if (slow == fast)
 			return (1);
 	}
-	
+
 	return (0);
 }

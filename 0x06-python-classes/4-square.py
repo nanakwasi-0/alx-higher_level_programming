4752561e7fe6037bb5f39749c167e7ea49b2dde6
nanakwasi-0 <nankwasi999@gmail.com>
2023-11-21 15:06:40 -0800

    4-fix 2

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
         """
         self.size = size
 
-        @property
+    @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
